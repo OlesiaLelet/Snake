13b6d18a68f671ea14263b2606bbc90f4186dfbc
Olesia <leletolesa@gmail.com>
2024-01-23 10:06:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    {<div className="Content">
+      <div className="box-1">1</div>
+      <div className="box-2">2</div>
+      <div className="box-3">3</div>
+      <div className="box-4">4</div>
+      <div className="box-5">5</div>
+      <div className="box-6">6</div>
+      </div>
+
+
+
+
+
+     }
+  </div>;
+}
+
+export default App;
